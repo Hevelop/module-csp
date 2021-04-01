a84fdbcf4a35851f84aed399201b6ce859f44c2c
Alex Bordin <alex@hevelop.com>
2021-04-01 14:58:37 +0200

    first commit

---
+++ registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Copyright © Hevelop srl. All rights reserved.
+ * @license https://opensource.org/licenses/agpl-3.0  AGPL-3.0 License
+ * @author Alex Bordin <alex@hevelop.com>
+ * @copyright Copyright (c) 2021 Hevelop srl (https://hevelop.com)
+ * @package Hevelop_Csp
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Hevelop_Csp',
+    __DIR__
+);
